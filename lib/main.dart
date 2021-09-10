100c68e5c65f1e9993bc57a50093ec5f8d11aac2
Aschwin B <aschwinbruyning@outlook.com>
2021-09-10 19:06:35 +0200

    + Add BlocProvider & event in ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:miniature_tb_game/bloc/battle_system/battle_system_bloc.dart';
+import 'package:miniature_tb_game/bloc/simple_bloc_observer.dart';
 import 'package:miniature_tb_game/ui/battle_screen.dart';
 
 void main() {
+  //Initialize Bloc Observer
+  Bloc.observer = SimpleBlocObserver();
+
+  //Start App
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<BattleSystemBloc>(
+          create: (_) => BattleSystemBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BattleScreen(),
       ),
-      home: BattleScreen(),
     );
   }
 }
--- lib/ui/battle_screen.dart
+++ lib/ui/battle_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:miniature_tb_game/bloc/battle_system/battle_system_bloc.dart';
 
 class BattleScreen extends StatefulWidget {
   const BattleScreen() : super();
@@ -8,6 +10,12 @@ class BattleScreen extends StatefulWidget {
 }
 
 class _BattleScreenState extends State<BattleScreen> {
+  @override
+  void initState() {
+    BlocProvider.of<BattleSystemBloc>(context).add(StartBattle());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
